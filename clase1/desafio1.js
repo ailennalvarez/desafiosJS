9e4244f9ce320c9c50c404c33571e083c0663f15
ailen alvarez <ailennalvarez4@gmail.com>
2021-10-04 23:56:03 -0300

    Desafio 1 JS

---
+++ clase1/desafio1.js
@@ -0,0 +1,11 @@
+let nombre = prompt ("Ingrese su nombre");
+console.log ("Hola" +" "+ nombre);
+let apellido = prompt("Ingrese su apellido");
+console.log ("Su nombre y apellido es" + " " + nombre + " "+ apellido);
+
+let edadUsuario = prompt("Ingrese su edad");
+
+let edadFinal = parseInt(edadUsuario) + 10;
+
+console.log (edadFinal);
+
