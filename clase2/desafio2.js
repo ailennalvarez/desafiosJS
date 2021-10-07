c5acda105ec038a927928fc32efbf0810ad16cf6
ailen alvarez <ailennalvarez4@gmail.com>
2021-10-07 13:09:34 -0300

    Desafio 2

---
+++ clase2/desafio2.js
@@ -0,0 +1,27 @@
+let numero = prompt("Ingrese un numero");
+
+let numeroFinal = parseInt(numero);
+
+if (numeroFinal > 100) {
+    console.log("El numero es " + numeroFinal);
+} else {
+    console.log("Ingrese otro numero");
+}
+
+let texto = prompt("Ingrese un saludo");
+
+if ((texto == "Hola") || (texto == "HOLA")) {
+    console.log("Bienvenido");
+}else{
+    console.log("Pruebe con otro saludo");
+}
+
+let otroNum = prompt("Ingrese un numero");
+
+let numFinal = parseInt(otroNum);
+
+if ((numFinal>10) && (numFinal<50)) {
+    console.log("El numero es correcto");
+} else {
+    console.log("Ingrese otro numero");
+}
